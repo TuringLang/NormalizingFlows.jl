c864d687acad6ba64a5242ea03dbcb1d7a3d6327
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-06-02 10:45:13 +0100

    minor changes to the generated ppackage

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# NormalizingFlows
+# NormalizingFlows.jl
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://torfjelde.github.io/NormalizingFlows.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://torfjelde.github.io/NormalizingFlows.jl/dev/)
 [![Build Status](https://github.com/torfjelde/NormalizingFlows.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/torfjelde/NormalizingFlows.jl/actions/workflows/CI.yml?query=branch%3Amain)
--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,13 @@
 using NormalizingFlows
 using Documenter
 
-DocMeta.setdocmeta!(NormalizingFlows, :DocTestSetup, :(using NormalizingFlows); recursive=true)
+DocMeta.setdocmeta!(
+    NormalizingFlows, :DocTestSetup, :(using NormalizingFlows); recursive=true
+)
 
 makedocs(;
     modules=[NormalizingFlows],
-    authors="Tor Erlend Fjelde <tor.erlend95@gmail.com> and contributors",
-    repo="https://github.com/torfjelde/NormalizingFlows.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/TuringLang/NormalizingFlows.jl/blob/{commit}{path}#{line}",
     sitename="NormalizingFlows.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
@@ -14,12 +15,7 @@ makedocs(;
         edit_link="main",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages=["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/torfjelde/NormalizingFlows.jl",
-    devbranch="main",
-)
+deploydocs(; repo="github.com/torfjelde/NormalizingFlows.jl", devbranch="main")
