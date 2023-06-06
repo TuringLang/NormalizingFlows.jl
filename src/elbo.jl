3b2b262ac2ec5052703e63e22dfbe1bd5792763c
zuhengxu <zuhengdavid@gmail.com>
2023-06-06 11:00:24 +0800

    edit elbo.jl based on comments

--- src/elbo.jl
+++ src/elbo.jl
@@ -6,76 +6,68 @@ using Random
 # training by minimizing reverse KL
 ####################################    
 function elbo_single_sample(
-    x::Union{AbstractVector{T}, T},                          # sample from reference dist q
-    flow::Bjectors.TransformedDistribution  # variational distribution to be trained
+    flow::Bjectors.TransformedDistribution,     # variational distribution to be trained
     logp,                                       # lpdf (unnormalized) of the target distribution
-    logq                                        # lpdf (exact) of the reference distribution
-    ) where {T<:Real}
+    x                                           # sample from reference dist q
+    ) 
     y, logabsdetjac = with_logabsdet_jacobian(flow.transform, x)
-    return logp(y) - logq(x) + logabsdetjac
+    return logp(y) - logpdfs(flow.dist, x) + logabsdetjac
 end
 
 # ELBO based on multiple iid samples
 function elbo(
-    xs::AbstractVector,                         # samples from reference dist q
     flow::Bijectors.UnivariateTransformed,      # variational distribution to be trained
     logp,                                       # lpdf (unnormalized) of the target distribution
-    logq                                        # lpdf (exact) of the reference distribution
+    xs::AbstractVector                          # samples from reference dist q
     )
-    n_samples = size(xs, 1) # each column is a sample
-    elbo_values = map(x -> elbo_single_sample(x, flow, logp, logq), xs)
+    elbo_values = map(x -> elbo_single_sample(flow, logp, x), xs)
     return mean(elbo_values)
 end
 
 function elbo(
-    xs::AbstractMatrix,                         # samples from reference dist q
     flow::Bijectors.MultivariateTransformed,    # variational distribution to be trained
     logp,                                       # lpdf (unnormalized) of the target distribution
-    logq                                        # lpdf (exact) of the reference distribution
+    xs::AbstractMatrix,                         # samples from reference dist q
     )
-    n_samples = size(xs, 2) # each column is a sample
-    elbo_values = map(x -> elbo_single_sample(x, flow, logp, logq), eachcol(xs))
+    elbo_values = map(x -> elbo_single_sample(flow, logp, x), eachcol(xs))
     return mean(elbo_values)
 end
 
-elbo(rng::AbstractRNG, flow::Bijectors.MultivariateTransformed, logp, logq, n_samples) = elbo(
-    rand(rng, flow.dist, n_samples), flow, logp, logq
+elbo(rng::AbstractRNG, flow::Bijectors.MultivariateTransformed, logp, n_samples) = elbo(
+    flow, logp, rand(rng, flow.dist, n_samples)
 )
 
-elbo(rng::AbstractRNG, flow::Bijectors.UnivariateTransformed, logp, logq, n_samples) = elbo(
-    rand(rng, flow.dist, n_samples), flow, logp, logq
+elbo(rng::AbstractRNG, flow::Bijectors.UnivariateTransformed, logp, n_samples) = elbo(
+    flow, logp, rand(rng, flow.dist, n_samples)
 )
 ####################################
 # training by minimizing forward KL (MLE)
 ####################################    
-
 function neg_llh_single_sample(
-    x::Union{AbstractVector{T}, T},                          # sample from target dist p
-    flow::Union{Bijectors.MultivariateTransformed, Bijectors.UnivariateTransformed},    # variational distribution to be trained
-    logq                                        # lpdf (exact) of the reference distribution
-    ) where {T<:Real}
+    flow::Bjectors.TransformedDistribution,     # variational distribution to be trained
+    logq,                                       # lpdf (exact) of the reference distribution
+    x,                                          # sample from target dist p
+    ) 
     b = inverse(flow.transform)
     y, logjac = with_logabsdet_jacobian(b, x)
     return -logq(y) - logjac
 end
     
 function neg_llh(
-    xs::AbstractVector,                       # sample from target dist p
     flow::Bijectors.UnivariateTransformed,    # variational distribution to be trained
-    logq                                      # lpdf (exact) of the reference distribution
+    logq,                                     # lpdf (exact) of the reference distribution
+    xs::AbstractVector,                       # sample from target dist p
     )
-    n_samples = size(xs, 1) # each column is a sample
-    neg_llhs = map(x -> neg_llh_single_sample(x, flow, logq), xs)
-    return sum(neg_llhs) / n_samples
+    neg_llhs = map(x -> neg_llh_single_sample(flow, logq, x), xs)
+    return mean(neg_llhs) 
 end
    
 function neg_llh(
-    xs::AbstractMatrix,                         # sample from target dist p
     flow::Bijectors.MultivariateTransformed,    # variational distribution to be trained
-    logq                                        # lpdf (exact) of the reference distribution
+    logq,                                        # lpdf (exact) of the reference distribution
+    xs::AbstractMatrix,                         # sample from target dist p
     )
-    n_samples = size(xs, 2) # each column is a sample
-    neg_llhs = map(x -> neg_llh_single_sample(x, flow, logq), eachcol(xs))
-    return sum(neg_llhs) / n_samples
+    neg_llhs = map(x -> neg_llh_single_sample(flow, logq, x), eachcol(xs))
+    return mean(neg_llhs)
 end
    
