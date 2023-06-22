4fd446370788cbcfe7f621bcd80c3b570e118657
David Xu <42751767+zuhengxu@users.noreply.github.com>
2023-06-22 11:12:54 -0700

    Update test/objectives.jl
    obj test on multiple types
    
    Co-authored-by: Tor Erlend Fjelde <tor.github@gmail.com>

--- test/objectives.jl
+++ test/objectives.jl
@@ -5,29 +5,31 @@ using NormalizingFlows
 using Test
 
 @testset "variational objectives" begin
-    μ = randn(Float32, 2)
-    Σ = Diagonal(rand(Float32, 2) .+ 1.0f-3)
-    target = MvNormal(μ, Σ)
-    logp(z) = logpdf(target, z)
+    @testset "$T" for T in [Float32, Float64]
+        μ = randn(T, 2)
+        Σ = Diagonal(rand(T, 2) .+ T(1e-3))
+        target = MvNormal(μ, Σ)
+        logp(z) = logpdf(target, z)
 
-    q₀ = MvNormal(zeros(Float32, 2), ones(Float32, 2))
-    flow = Bijectors.transformed(q₀, Bijectors.Shift(μ) ∘ Bijectors.Scale(sqrt.(Σ)))
+        q₀ = MvNormal(zeros(T, 2), ones(T, 2))
+        flow = Bijectors.transformed(q₀, Bijectors.Shift(μ) ∘ Bijectors.Scale(sqrt.(Σ)))
 
-    x = randn(Float32, 2)
+        x = randn(T, 2)
 
-    @testset "elbo" begin
-        el = elbo(Random.default_rng(), flow, logp, 10)
+        @testset "elbo" begin
+            el = elbo(Random.default_rng(), flow, logp, 10)
 
-        @test abs(el) < 1.0f-5
-        @test logpdf(flow, x) + el ≈ logp(x)
-    end
+            @test el ≈ atol=1e-5
+            @test logpdf(flow, x) + el ≈ logp(x)
+        end
 
-    @testset "likelihood" begin
-        sample_trained = rand(flow, 1000)
-        sample_untrained = rand(q₀, 1000)
-        llh_trained = loglikelihood(flow, sample_trained)
-        llh_untrained = loglikelihood(flow, sample_untrained)
+        @testset "likelihood" begin
+            sample_trained = rand(flow, 1000)
+            sample_untrained = rand(q₀, 1000)
+            llh_trained = loglikelihood(flow, sample_trained)
+            llh_untrained = loglikelihood(flow, sample_untrained)
 
-        @test llh_trained > llh_untrained
+            @test llh_trained > llh_untrained
+        end
     end
 end
