2a8ac3f57839a21c22af89f0fdb6f1ae2402f704
zuheng <zuhengdavid@gmail.com>
2023-06-27 13:04:51 -0700

    minor fix test error

--- test/ad.jl
+++ test/ad.jl
@@ -29,7 +29,7 @@ end
     ]
         @testset "$T" for T in [Float32, Float64]
             μ = 10 * ones(T, 2)
-            Σ = Diagonal(D .^ 2)
+            Σ = Diagonal(4 * ones(T, 2))
             target = MvNormal(μ, Σ)
             logp(z) = logpdf(target, z)
 
