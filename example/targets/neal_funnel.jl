4b8c336e3edf5859a673d8b79d6888479dca42f5
Zuheng <zuhengdavid@gmail.com>
2023-07-18 15:49:46 -0700

    add Neal's Funnel and Warped Gaussian

---
+++ example/targets/neal_funnel.jl
@@ -0,0 +1,62 @@
+using Distributions, Random
+
+"""
+    Funnel{T<:Real}
+
+Multidimensional Neal's Funnel distribution
+
+# Fields 
+- 'dim::Int': Dimension of the distribution, must be >= 2
+- 'μ::T': Mean of the first dimension
+- 'σ::T': Standard deviation of the first dimension, must be > 0
+
+# Explanation
+
+The Neal's Funnel distribution is a p-dimensional distribution with a funnel shape, 
+originally proposed by Radford Neal in [2]. 
+The marginal distribution of "x₁" is Gaussian with mean "μ" and standard
+deviation "σ". The conditional distribution of "x₂, …, xₚ" given "x₁" is independent 
+Gaussian distribution with mean 0 and standard deviation "exp(x₁/2)". 
+The generative process is given by
+"x₁ ∼ N(μ, σ²), x₂, ..., xₚ ∼ N(0, exp(x₁))"
+
+
+# Reference
+[1] Stan User’s Guide: 
+https://mc-stan.org/docs/2_18/stan-users-guide/reparameterization-section.html#ref-Neal:2003
+[2] Radford Neal 2003. “Slice Sampling.” Annals of Statistics 31 (3): 705–67.
+
+"""
+struct Funnel{T<:Real} <: ContinuousMultivariateDistribution
+    dim::Int      # Dimension
+    μ::T          # Mean of the first dimension
+    σ::T          # sd of the first dimension
+    function Funnel{T}(dim::Int, μ::T, σ::T) where {T<:Real}
+        dim >= 2 || error("dim must be >= 2")
+        σ > 0 || error("σ must be > 0")
+        return new{T}(dim, μ, σ)
+    end
+end
+Funnel(dim::Int, μ::T, σ::T) where {T<:Real} = Funnel{T}(dim, μ, σ)
+Funnel(dim::Int, σ::T) where {T<:Real} = Funnel{T}(dim, zero(T), σ)
+Funnel(dim::Int) = Funnel(dim, 0.0, 9.0)
+
+Base.length(p::Funnel) = p.dim
+Base.eltype(p::Funnel{T}) where {T<:Real} = T
+Distributions.sampler(p::Funnel) = p
+
+function Distributions._rand!(rng::AbstractRNG, p::Funnel, x::AbstractVecOrMat)
+    T = eltype(x)
+    d, μ, σ = p.dim, p.μ, p.σ
+    d == size(x, 1) || error("Dimension mismatch")
+    x[1, :] .= randn(rng, T, size(x, 2)) .* σ .+ μ
+    x[2:end, :] .= randn(rng, T, d - 1, size(x, 2)) .* exp.(@view(x[1, :]) ./ 2)'
+    return x
+end
+
+function Distributions._logpdf(p::Funnel, x::AbstractVector)
+    d, μ, σ = p.dim, p.μ, p.σ
+    lpdf1 = logpdf(Normal(μ, σ), x[1])
+    lpdfs = logpdf.(Normal.(zeros(T, d - 1), exp(x[1] / 2)), @view(x[2:end]))
+    return lpdf1 + sum(lpdfs)
+end
---
+++ example/targets/warped_gaussian.jl
@@ -0,0 +1,86 @@
+using Distributions, Random, LinearAlgebra, IrrationalConstants
+
+"""
+    WarpedGauss{T<:Real}
+
+2-dimensional warped Gaussian distribution
+
+# Fields 
+- 'σ₁::T': Standard deviation of the first dimension, must be > 0
+- 'σ₂::T': Standard deviation of the second dimension, must be > 0 
+
+# Explanation
+
+The banana distribution is obtained by applying a transformation ϕ to a 2-dimensional normal 
+distribution "N(0, diag(σ₁, σ₂))". The transformation ϕ(x) is defined as
+"ϕ(x₁, x₂) = (r*cos(θ + r/2), r*sin(θ + r/2))", 
+where "r = sqrt(x₁² + x₂²)", "θ = atan(x₂, x₁)", 
+and "atan(y, x) ∈ [-π, π]" is the angle, in radians, between the positive x axis and the 
+ray to the point "(x, y)".
+
+
+# Reference
+[1] Zuheng Xu, Naitong Chen, Trevor Campbell
+"MixFlows: principled variational inference via mixed flows."
+International Conference on Machine Learning, 2023
+"""
+struct WarpedGauss{T<:Real} <: ContinuousMultivariateDistribution
+    σ₁::T          # sd of the first dimension
+    σ₂::T          # sd of the second dimension   
+    function WarpedGauss{T}(σ₁, σ₂) where {T<:Real}
+        σ₁ > 0 || error("σ₁ must be > 0")
+        σ₂ > 0 || error("σ₂ must be > 0")
+        return new{T}(σ₁, σ₂)
+    end
+end
+WarpedGauss(σ₁::T, σ₂::T) where {T<:Real} = WarpedGauss{T}(σ₁, σ₂)
+WarpedGauss() = WarpedGauss(1.0, 0.12)
+
+Base.length(p::WarpedGauss) = 2
+Base.eltype(p::WarpedGauss{T}) where {T<:Real} = T
+Distributions.sampler(p::WarpedGauss) = p
+
+# Define the transformation function φ and the inverse ϕ⁻¹ for the warped Gaussian distribution
+function ϕ!(p::WarpedGauss, z::AbstractVector)
+    length(z) == 2 || error("Dimension mismatch")
+    x, y = z
+    r = norm(z)
+    θ = atan(y, x) #in [-π , π]
+    θ -= r / 2
+    z .= r .* [cos(θ), sin(θ)]
+    return z
+end
+
+function ϕ⁻¹(p::WarpedGauss, z::AbstractVector)
+    length(z) == 2 || error("Dimension mismatch")
+    x, y = z
+    r = norm(z)
+    θ = atan(y, x) #in [-π , π]
+    # increase θ depending on r to "smear"
+    θ += r / 2
+
+    # get the x,y coordinates foαtransformed point
+    xn = r * cos(θ)
+    yn = r * sin(θ)
+    # compute jacobian
+    logJ = log(r)
+    return [xn, yn], logJ
+end
+
+function Distributions._rand!(rng::AbstractRNG, p::WarpedGauss, x::AbstractVecOrMat)
+    size(x, 1) == 2 || error("Dimension mismatch")
+    σ₁, σ₂ = p.σ₁, p.σ₂
+    randn!(rng, x) .* [σ₁, σ₂]
+    for y in eachcol(x)
+        ϕ!(p, y)
+    end
+    return x
+end
+
+function Distributions._logpdf(p::WarpedGauss, x::AbstractVector)
+    size(x, 1) == 2 || error("Dimension mismatch")
+    σ₁, σ₂ = p.σ₁, p.σ₂
+    S = [σ₁, σ₂] .^ 2
+    z, logJ = ϕ⁻¹(p, x)
+    return -sum(z .^ 2 ./ S) / 2 - IrrationalConstants.log2π - log(σ₁) - log(σ₂) + logJ
+end
