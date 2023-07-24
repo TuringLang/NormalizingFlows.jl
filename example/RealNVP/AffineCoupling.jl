7964f917cb85ab548c3a855d3473f25291c0e188
Zuheng <zuhengdavid@gmail.com>
2023-07-23 22:02:28 -0700

    minor ed

--- example/RealNVP/AffineCoupling.jl
+++ example/RealNVP/AffineCoupling.jl
@@ -1,9 +1,8 @@
 using Flux
 using Functors
 using Bijectors
-using Bijectors:∘, partition, combine, PartitionMask
-import Bijectors:transform,with_logabsdet_jacobian,logabsdetjac
-
+using Bijectors: ∘, partition, combine, PartitionMask
+import Bijectors: transform, with_logabsdet_jacobian, logabsdetjac
 
 """
 Affinecoupling layer for RealNVP "(http://proceedings.mlr.press/v118/fjelde20a/fjelde20a.pdf)"
@@ -21,18 +20,26 @@ end
 function AffineCoupling(
     D::Int,  # dimension of input
     hdims::Int, # dimension of hidden units for s and t
-    mask_idx::AbstractVector # index of dimensione that one wants to apply transformations on
-    )
+    mask_idx::AbstractVector, # index of dimensione that one wants to apply transformations on
+)
     cdims = D ÷ 2 # D is even (and D ≥ 4 since PartitionMask requires D ≥ 3)
-    s = Chain(Flux.Dense(cdims, hdims, leakyrelu), Flux.Dense(hdims, hdims, leakyrelu), Flux.Dense(hdims, cdims))
-    t = Chain(Flux.Dense(cdims, hdims, leakyrelu), Flux.Dense(hdims, hdims, leakyrelu), Flux.Dense(hdims, cdims))
+    s = Chain(
+        Flux.Dense(cdims, hdims, leakyrelu),
+        Flux.Dense(hdims, hdims, leakyrelu),
+        Flux.Dense(hdims, cdims),
+    )
+    t = Chain(
+        Flux.Dense(cdims, hdims, leakyrelu),
+        Flux.Dense(hdims, hdims, leakyrelu),
+        Flux.Dense(hdims, cdims),
+    )
     Mask = Bijectors.PartitionMask(D, mask_idx)
-    AffineCoupling(D, Mask, s, t)
+    return AffineCoupling(D, Mask, s, t)
 end
 
 function Bijectors.transform(af::AffineCoupling, x::AbstractVector)
     # partition vector using 'af.Mask::PartitionMask`
-    x₁, x₂, x₃= Bijectors.partition(af.Mask, x)
+    x₁, x₂, x₃ = Bijectors.partition(af.Mask, x)
     y₁ = x₁ .* exp.(af.s(x₂)) .+ af.t(x₂)
     return Bijectors.combine(af.Mask, y₁, x₂, x₃)
 end
@@ -48,7 +55,6 @@ function Bijectors.with_logabsdet_jacobian(af::AffineCoupling, x::AbstractVector
     return combine(af.Mask, y_1, x_2, x_3), logjac
 end
 
-
 function Bijectors.transform(iaf::Inverse{<:AffineCoupling}, y::AbstractVector)
     af = iaf.orig
     # partition vector using `af.mask::PartitionMask`
@@ -58,7 +64,9 @@ function Bijectors.transform(iaf::Inverse{<:AffineCoupling}, y::AbstractVector)
     return combine(af.Mask, x_1, y_2, y_3)
 end
 
-function Bijectors.with_logabsdet_jacobian(iaf::Inverse{<:AffineCoupling}, y::AbstractVector)
+function Bijectors.with_logabsdet_jacobian(
+    iaf::Inverse{<:AffineCoupling}, y::AbstractVector
+)
     af = iaf.orig
     # partition vector using `af.mask::PartitionMask`
     y_1, y_2, y_3 = partition(af.Mask, y)
@@ -68,16 +76,11 @@ function Bijectors.with_logabsdet_jacobian(iaf::Inverse{<:AffineCoupling}, y::Ab
     return combine(af.Mask, x_1, y_2, y_3), logjac
 end
 
-
 function Bijectors.logabsdetjac(af::AffineCoupling, x::AbstractVector)
     x_1, x_2, x_3 = partition(af.Mask, x)
     logjac = sum(af.s(x_2))
     return logjac
 end
-# Todo: specialized method for 2d input
-# `partition` does support 2d input 
-
-
 
 # # test invertibility
 # Ls = [
@@ -101,7 +104,6 @@ end
 
 # using Zygote
 # Zygote.gradient(test, x0)
-
 
 # X = randn(2)
 # mask = Bijectors.PartitionMask(2, 1:1)
