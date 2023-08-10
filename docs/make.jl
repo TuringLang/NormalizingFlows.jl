b6232581acff4730b52a9b28844e1ebda4d69eeb
Zuheng <zuhengdavid@gmail.com>
2023-08-09 22:30:43 -0700

    set up doc files

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,12 @@ makedocs(;
     repo="https://github.com/TuringLang/NormalizingFlows.jl/blob/{commit}{path}#{line}",
     sitename="NormalizingFlows.jl",
     format=Documenter.HTML(),
-    pages=["Home" => "index.md"],
+    pages=[
+        "Home" => "index.md",
+        "API" => "api.md",
+        "Examples" => "example.md",
+        "Customize your own flow layer" => "custommized_layer.md",
+    ],
 )
 
 deploydocs(; repo="github.com/TuringLang/NormalizingFlows.jl", devbranch="main")
