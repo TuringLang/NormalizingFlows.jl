a67a6e2ac5d1fc39206789c15b69a8b5c9a85cb7
zuhengxu <zuhengdavid@gmail.com>
2023-10-09 23:12:14 -0700

    update deep mlp

--- example/MLP/main.jl
+++ example/MLP/main.jl
@@ -18,10 +18,10 @@ rng = Random.default_rng()
 include("../targets/banana.jl")
 
 # create target p
-p = Banana(2, 5.0f-1, 100.0f0)
+p = Banana(2, 3.0f-1, 100.0f0)
 logp = Base.Fix1(logpdf, p)
 
-Data = rand(p, 20000)
+Data = rand(p, 60000)
 data_load = Flux.DataLoader(Data; batchsize=200, shuffle=true)
 
 ######################################
@@ -31,11 +31,16 @@ d = p.dim
 μ = zeros(Float32, d)
 Σ = I
 
-Ls = [InvertibleMLP(d) ∘ Flux._paramtype(Float32, PlanarLayer(2)) for i in 1:20]
+Ls = [
+    InvertibleMLP(d) ∘ Flux._paramtype(Float32, PlanarLayer(2)) ∘
+    Flux._paramtype(Float32, RadialLayer(2)) for i in 1:17
+]
 ts = fchain(Ls)
 q0 = MvNormal(μ, Σ)
 flow = transformed(q0, ts)
+@functor MvNormal ()
 flow_untrained = deepcopy(flow)
+
 ######################
 # train the flow
 #####################
@@ -60,18 +65,23 @@ function train(flow, data_load, opt, n_epoch=100)
     return flow_trained, opt_stats, st
 end
 
-flow_trained, stats, _ = train(flow, data_load, Optimisers.ADAM(1e-3), 1000)
-pt = compare_trained_and_untrained_flow_BN(flow_trained, flow_untrained, p, 1000)
-plot!(; xlims=(-50, 50), ylims=(-100, 20))
+flow_trained, stats, _ = train(flow, data_load, Optimisers.ADAM(9e-4), 1500)
 
-# stability
-setprecision(BigFloat, 256)
+using JLD2
+JLD2.save("res/big_MLP.jld2", "model", flow_trained)
+
+res = JLD2.load("res/big_MLP.jld2")
+flow_trained = res["model"]
+pt = compare_trained_and_untrained_flow_BN(flow_trained, flow_untrained, p, 1000)
+
+# # stability
+setprecision(BigFloat, 2048)
 ft = BigFloat
 
 @functor MvNormal
 flow_big = Flux._paramtype(BigFloat, flow_trained)
 
-Xs = randn(Float32, 2, 1000)
+Xs = randn(Float32, d, 1000)
 Xs_big = ft.(Xs)
 ts = flow_trained.transform
 ts_big = flow_big.transform
@@ -79,13 +89,18 @@ its = inverse(flow_trained.transform)
 its_big = inverse(flow_big.transform)
 
 diff = ts(Xs) .- ts_big(Xs_big)
-dd = map(norm, eachcol(diff))
+dd = Float32.(map(norm, eachcol(diff)))
 
 # density error
-
 Ys = rand(p, 1000)
 Ys_big = ft.(Ys)
 
 diff_inv = its(Ys) .- its_big(Ys_big)
+dd_inv = Float32.(map(norm, eachcol(diff_inv)))
 
-logpdf(flow_trained, Ys) .- logpdf(flow_big, Ys_big)
+lpdfs_err = abs.(Float32.((logpdf(flow_trained, Ys) .- logpdf(flow_big, Ys_big))))
+abs.(
+    Float32.(
+        (logpdf(flow_trained, Ys) .- logpdf(flow_big, Ys_big)) ./ logpdf(flow_big, Ys_big)
+    )
+)
