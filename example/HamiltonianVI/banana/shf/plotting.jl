2ae4f2704b3dc2a36c7bb10c002a823b5c1875cc
zuhengxu <zuhengdavid@gmail.com>
2023-10-17 22:14:31 -0700

    update banana plots

--- example/HamiltonianVI/banana/shf/plotting.jl
+++ example/HamiltonianVI/banana/shf/plotting.jl
@@ -371,3 +371,39 @@ plot!(;
 )
 
 savefig(p1, "figure/window_log.png")
+
+# window computation times 
+T_fwd = res_shadowing["T_fwd"]
+T_bwd = res_shadowing["T_bwd"]
+
+p1 = plot(
+    1:nlayers,
+    vec(median(T_fwd; dims=2));
+    ribbon=get_percentiles(T_fwd; byrow=true),
+    lw=3,
+    label="Fwd",
+    xlabel="#transformations",
+    ylabel="Wall time in sec.",
+)
+plot!(
+    1:nlayers,
+    vec(median(T_bwd; dims=2));
+    ribbon=get_percentiles(T_bwd),
+    lw=3,
+    label="Bwd",
+    xlabel="#transformations",
+    ylabel="",
+)
+plot!(;
+    title="ϵ computation time",
+    size=(800, 500),
+    xtickfontsize=30,
+    ytickfontsize=30,
+    margin=10Plots.mm,
+    guidefontsize=30,
+    legendfontsize=20,
+    titlefontsize=30,
+    legend=:topleft,
+)
+
+savefig(p1, "figure/time.png")
