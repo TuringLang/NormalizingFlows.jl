e68ef5f67e9ae2403fa6e8bc67081d58f242bd88
Zuheng <zuhengdavid@gmail.com>
2025-02-16 15:46:38 -0800

    rm all ext for now

--- src/NormalizingFlows.jl
+++ src/NormalizingFlows.jl
@@ -11,8 +11,7 @@ using DocStringExtensions
 
 export train_flow, elbo, loglikelihood
 
-"""
-    train_flow([rng::AbstractRNG, ]vo, flow, args...; kwargs...)
+""" train_flow([rng::AbstractRNG, ]vo, flow, args...; kwargs...)
 
 Train the given normalizing flow `flow` by calling `optimize`.
 
