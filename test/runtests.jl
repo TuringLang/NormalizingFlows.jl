104e5dddb31d4d9c44779d9f792de5594718b403
Zuheng <zuhengdavid@gmail.com>
2025-02-16 17:06:50 -0800

    minor ed

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Bijectors, Optimisers
 using LinearAlgebra
 using Random
 using ADTypes
+using Functors
 # import DifferentiationInterface as DI
 using ForwardDiff, Zygote, ReverseDiff, Mooncake
 using Test
